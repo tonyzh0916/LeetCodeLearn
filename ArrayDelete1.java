cb62768f1c8781a839340e62c33d44357776a767
Yao Zhang <Yao.Zhang@gartner.com>
2020-12-11 16:06:11 -0500

    finish delete

--- ArrayDelete1.java
+++ ArrayDelete1.java
@@ -5,11 +5,19 @@ import java.util.Arrays;
 public class ArrayDelete1 {
 
 	public static void main(String[] args) {
-		int[] nums = {0,2,2,0};
-		int val = 0;
-		System.out.println(removeElement(nums, val));
+		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+		int val = 2;
+		System.out.println(removeDuplicates(nums));
 
 	}
+	
+	
+	/*
+	 * 
+	 * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+	   Output: 5, nums = [0,1,4,0,3]
+
+	 */
 	
 	public static int removeElement(int[] nums, int val) {
 	    int i = 0;
@@ -22,5 +30,26 @@ public class ArrayDelete1 {
 	    System.out.println(Arrays.toString(nums));
 	    return i;
 	}
+	
+	
+//	Input: nums = [0,0,1,1,1,2,2,3,3,4]
+//	Output: 5, nums = [0,1,2,3,4]
+	
+    public static int removeDuplicates(int[] nums) {
+    	int k=0;
+        for(int i=0; i<nums.length;i++) {
+        	if(nums[k]!=nums[i]) {
+        		k++;
+        		nums[k]=nums[i];
+        		
+        	}else {
+        		continue;
+        	}
+        }
+        System.out.println(Arrays.toString(nums));
+        return k+1;
+    }
 
 }
+
+// two pointer one is fast one is slow 
