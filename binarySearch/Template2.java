9afd5757a2fc8052273d570b2603b497caeef015
Yao Zhang <Yao.Zhang@gartner.com>
2021-02-05 17:28:45 -0500

    binary search tamplate 2

--- binarySearch/Template2.java
+++ binarySearch/Template2.java
@@ -20,3 +20,36 @@
     }
 
     ///////////////////////
+    //Find Peak Element
+    class Solution {
+      public int findPeakElement(int[] nums) {
+          int left=0;
+          int right= nums.length-1;
+          while(left+1<right){
+              int mid= left+(right-left)/2;
+              if(nums[mid]>nums[mid+1]){
+                  right=mid;
+              }else{
+                  left=mid;
+              }
+          }
+          if(nums[left]>nums[right]){
+              return left;
+          }else{
+              return right;
+          }
+      }
+  }
+
+  //soluton 2
+  class Solution {
+    public int findMin(int[] nums) {
+        return findMin(nums,0,nums.length-1);
+    }
+    private int findMin(int[] nums, int left, int right){
+        if(left+1>=right) return Math.min(nums[left], nums[right]);
+        if(nums[left]<nums[right]) return nums[left];
+        int mid = left+(right-left)/2;
+        return Math.min(findMin(nums,left, mid-1), findMin(nums, mid,right));
+    }
+}
