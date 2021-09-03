89312cfaba8b6a64c2f91fe599bcd06f89b8dcbc
Yao Zhang <Yao.Zhang@gartner.com>
2021-09-02 23:00:39 -0400

    keep linkedList

--- leetcodeExplore/LinkedList/RemoveLinkedListElements.java
+++ leetcodeExplore/LinkedList/RemoveLinkedListElements.java
@@ -47,3 +47,18 @@ class Solution {
       return dummy.next;
   }
 }
+
+/////////////// practice /////////
+public ListNode removeElements2(ListNode head, int val){
+  if(head==null) return head;
+  ListNode dummy = new ListNode(-1);
+  dummy.next = head;
+  ListNode pointer = dummy;
+  while(pointer.next!=null){
+    if(pointer.next.val == val){
+      pointer.next = pointer.next.next;
+    }else{
+      pointer = pointer.next;
+    }
+  }
+}
