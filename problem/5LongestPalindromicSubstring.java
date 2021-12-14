9d870654b38cba05660ba76537011ee75d81507e
Yao Zhang <Yao.Zhang@gartner.com>
2021-12-13 22:13:16 -0500

    learned 45 and review 33 5

--- problem/5LongestPalindromicSubstring.java
+++ problem/5LongestPalindromicSubstring.java
@@ -66,3 +66,31 @@ class Solution{
         }
     }
 }
+
+/// 12/13/2021
+class Solution{
+    int maxLen = 0;
+    int left = 0;
+
+    public String longestPalindrome(String s){
+        if(s==null || s.length()<=1) return  s;
+
+        for(int i=0; i<s.length(); i++){
+            helper(s,i,i);
+            helper(s, i, i+1);
+        }
+        return s.substring(left, left+maxLen);
+    }
+
+    private void helper(String s, int l, int r){
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            if(r+1-left > maxLen){
+                maxLen=r-l+1;
+                left=l;
+            }
+            l--;
+            r++;
+        }
+    }
+
+}
