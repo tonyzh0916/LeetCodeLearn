68a2f83408f867fb9e87d3abdb324863719ca026
Yao Zhang <Yao.Zhang@gartner.com>
2021-12-14 21:56:26 -0500

    learned new and review

--- problem/35 *searchInseertPosstion.java
+++ problem/35 *searchInseertPosstion.java
@@ -79,3 +79,31 @@ class Solution{
     }
   }
 }
+
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+    if(nums == null || nums.length == 0) return 0;
+
+    int left = 0;
+    int right = nums.length-1;
+
+    while(left+1<right){
+      int mid = left +(right-left)/2;
+      if(nums[mid]==target) return mid;
+
+      if(nums[mid]<target){
+        left = mid;
+      }else{
+        right = mid;
+      }
+    }
+
+    if(nums[left]>=target){
+      return left;
+    }else if(nums[left]<target && target<=nums[right]){
+      return right;
+    }else{
+      return nums.length;
+    }
+  }
+}
