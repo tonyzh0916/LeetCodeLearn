fa8c17fb78de4db591bfb5ba56303392ad5c977a
Yao Zhang <Yao.Zhang@gartner.com>
2021-12-20 21:32:08 -0500

    learned 49

---
+++ problem/49 GroupAnaGrams.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+      HashMap<String, List<String>> map = new HashMap<>();
+      for(String str: strs){
+          String sorted = helper(str);
+          if(!map.containsKey(sorted)){
+              map.put(sorted, new ArrayList<>());
+          }
+          map.get(sorted).add(str);
+      }
+
+      List<List<String>> res = new ArrayList<>();
+      for(List<String> temp : map.values()){
+          res.add(temp);
+      }
+      
+      return res;
+  }
+  
+  private String helper(String s){
+      char[] charArr = s.toCharArray();
+      Arrays.sort(charArr);
+      return new String(charArr);
+  }
+}
