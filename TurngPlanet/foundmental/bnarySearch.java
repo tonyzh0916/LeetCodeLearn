4a8fccb9cce5abaae80f2e4b276491b43d68e4bb
Yao Zhang <Yao.Zhang@gartner.com>
2022-02-24 21:54:58 -0500

    111 and 112 plus turing

---
+++ TurngPlanet/foundmental/bnarySearch.java
@@ -0,0 +1,15 @@
+public int binarySearch(int[] array, int target){
+  int left =0;
+  int right = array.length-1;
+  while(left<=right){
+    int mid = left + (right-left)/2;
+    if(array[mid]==target){
+      return mid;
+    }else if(target<array[mid]){
+      right = mid-1;
+    }else{
+      left = mid+1;
+    }
+  } 
+  return -1;
+}
