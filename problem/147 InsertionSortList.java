9f0620af6828e2a9bd1c018d3f4060b2bc03ae61
Yao Zhang <Yao.Zhang@gartner.com>
2022-03-25 15:40:58 -0400

    147 150

---
+++ problem/147 InsertionSortList.java
@@ -0,0 +1,40 @@
+/*
+Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+
+The steps of the insertion sort algorithm:
+
+Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list 
+and inserts it there.
+It repeats until no input elements remain.
+The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the 
+first element in the list. 
+One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
+*/
+
+class Solution {
+  public ListNode insertionSortList(ListNode head) {
+    if(head ==null || head.next==null) return head;
+      ListNode dummy = new ListNode(-1);
+      dummy.next = head;
+      ListNode cur = head;
+      ListNode prev = null;
+      ListNode temp = null;
+
+      while(cur!=null && cur.next!=null){
+        if(cur.val <=cur.next.val){
+          cur = cur.next;
+        }else{
+          temp = cur.next;
+          cur.next =cur.next.next;
+          prev = dummy;
+          while(prev.next.val <=temp.val){
+            prev = prev.next;
+          }
+          temp.next = prev.next;
+          prev.next = temp;
+        }
+      }
+      return dummy.next;
+  }
+}
