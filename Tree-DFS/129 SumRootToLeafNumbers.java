6cb27cb9da73967db8f9289bde4d516dde137727
Yao Zhang <Yao.Zhang@gartner.com>
2022-08-16 23:20:28 -0400

    recursion

--- Tree-DFS/129 SumRootToLeafNumbers.java
+++ Tree-DFS/129 SumRootToLeafNumbers.java
@@ -1,3 +1,22 @@
-public class 129 SumRootToLeafNumbers {
-  
+int sum = 0;
+public int  sumNumbers(TreeNode root){
+  if(root ==null){
+    return 0;
+  }
+  dfs(root , 0);
+  return sum;
+}
+
+private void dfs(TreeNode root, int num){
+  num = num*10 + root .val;
+  if(root.left ==  null && root.right==null){
+    sum += num;
+    return;
+  }
+  if(root.left !=null){
+    dfs(root.left, num);
+  }
+  if(root.right !=null){
+    dfs(root.right, num);
+  }
 }
