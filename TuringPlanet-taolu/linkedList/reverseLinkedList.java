e7b00ba6cd4d9e70106f2640805ed7d8063cac30
Yao Zhang <yaozhang@Yaos-MBP.lan>
2022-09-07 23:44:25 -0400

    practice

--- TuringPlanet-taolu/linkedList/reverseLinkedList.java
+++ TuringPlanet-taolu/linkedList/reverseLinkedList.java
@@ -8,4 +8,21 @@ class Solution{
     head.next = null;
     return reverse_head;
   }
+
+  private ListNode reverse(ListNode head){
+    ListNode prev = null;
+    ListNode curr = head;
+    ListNode next = null;
+    
+    while(curr!=null){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
 }
+}
+
+/////
+
