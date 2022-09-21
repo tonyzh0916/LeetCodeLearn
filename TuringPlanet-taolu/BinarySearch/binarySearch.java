02d1213e9ac090c3037e3046e653d3d72a16b77f
Yao Zhang <Yao.Zhang@gartner.com>
2022-09-20 23:10:40 -0400

    binary search again

---
+++ TuringPlanet-taolu/BinarySearch/binarySearch.java
@@ -0,0 +1,18 @@
+package TuringPlanet-taolu.BinarySearch;
+
+class Solution{
+  public int binarySearch(int[] arr,  int k){
+    int l =0, r=arr.length-1;
+    while(l<=r){
+      int mid = l +(r-l)/2;
+      if(arr[mid]>k){
+        r = mid-1;
+      }else if(arr[mid]==k){
+        return mid;
+      }else{
+        l=mid+1;
+      }
+    }
+    return -1;
+  }
+}
