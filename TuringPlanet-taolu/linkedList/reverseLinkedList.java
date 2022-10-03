f7212ce31afaeff952c41455a7257856514e5a85
Yao Zhang <yaozhang@Yaos-MBP.lan>
2022-10-03 00:00:27 -0400

    linkedList

--- TuringPlanet-taolu/linkedList/reverseLinkedList.java
+++ TuringPlanet-taolu/linkedList/reverseLinkedList.java
@@ -8,4 +8,15 @@ class Solution{
     head.next = null;
     return reverse_head;
   }
+
+  public ListNode reverse2(ListNode head){
+    if(head ==null || head.next == null){
+      return head;
+    }
+
+    ListNode reversed_head = reverse2(head.next);
+    head.next.next = head;
+    head.next=null;
+    return reversed_head;
+  }
 }
