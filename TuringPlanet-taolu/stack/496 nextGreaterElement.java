8ebec197124e976d1118ee3e3597bfdb685ab3dd
Yao Zhang <yaozhang@Yaos-MBP.lan>
2022-10-27 22:06:39 -0400

    learning

--- TuringPlanet-taolu/stack/496 nextGreaterElement.java
+++ TuringPlanet-taolu/stack/496 nextGreaterElement.java
@@ -19,4 +19,23 @@ class Solution{
 
         return nums1;
     }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : nums2){
+            if(!stack.isEmpty() && stack.peek()<num){
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        for(int i=0; i<nums1.length;i++){
+            nums1[i]=map.getOrDefault(nums1[i], -1);
+        }
+
+        return nums1;
+    }
+
 }
